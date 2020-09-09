405c84b9fc1a0835000145397114ae52b3299300
Caleb Ledi <caleb.gorrila@gmail.com>
2020-09-10 00:23:53 +0700

    Passport Facebook:

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2874176459479850',
+        clientSecret: 'dda869815547aacc750951d2126fa6dd'
+    }
 }
